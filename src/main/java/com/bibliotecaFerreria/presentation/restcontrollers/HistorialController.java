6d947a10038bef5c7be46e0e4afa660b06b5e452
AlexLopezGarcia <alex.lopez.vortex@gmail.com>
2025-02-18 19:20:13 +0100

    Update HistorialController.java

--- src/main/java/com/bibliotecaFerreria/presentation/restcontrollers/HistorialController.java
+++ src/main/java/com/bibliotecaFerreria/presentation/restcontrollers/HistorialController.java
@@ -3,6 +3,8 @@ package com.bibliotecaFerreria.presentation.restcontrollers;
 import com.bibliotecaFerreria.bussiness.model.Historial;
 import com.bibliotecaFerreria.bussiness.model.HistorialDTO;
 import com.bibliotecaFerreria.bussiness.services.HistorialServicesImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,9 +22,19 @@ import java.util.stream.Collectors;
 @RequestMapping("/historial")
 @Tag(name = "Historial", description = "Gestión de historial de usuarios")
 public class HistorialController {
+
     @Autowired
     private HistorialServicesImpl historialServices;
 
+    @Operation (
+            summary = "Obtener todos los registros de historial",
+            description = "Devuelve todos los registros de historial",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Registros de historial encontrados"),
+                    @ApiResponse(responseCode = "404", description = "Registro de historial no encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Error interno")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<HistorialDTO>> getAll() {
         List<HistorialDTO> historialDTOs = historialServices.getAll().stream()
@@ -31,6 +43,15 @@ public class HistorialController {
         return ResponseEntity.ok(historialDTOs);
     }
 
+    @Operation (
+            summary = "Obtener un registro de historial",
+            description = "Devuelve un registro de historial por su ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Registro de historial encontrado"),
+                    @ApiResponse(responseCode = "404", description = "Registro de historial no encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Error interno")
+            }
+    )
     @GetMapping("/{historialId}")
     public ResponseEntity<?> read(@PathVariable Long historialId) {
         return historialServices.read(historialId)
@@ -38,6 +59,14 @@ public class HistorialController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation (
+            summary = "Crear un registro de historial",
+            description = "Crea un registro de historial",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Registro de historial creado"),
+                    @ApiResponse(responseCode = "500", description = "Error interno")
+            }
+    )
     @PostMapping
     public ResponseEntity<String> create(@RequestBody Historial historial) {
         Long id = historialServices.create(historial);
@@ -45,6 +74,15 @@ public class HistorialController {
                 .body("Registro de historial creado con ID: " + id);
     }
 
+    @Operation (
+            summary = "Actualizar un registro de historial",
+            description = "Actualiza un registro de historial",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Registro de historial actualizado"),
+                    @ApiResponse(responseCode = "404", description = "Registro de historial no encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Error interno")
+            }
+    )
     @PutMapping("/{historialId}")
     public ResponseEntity<String> update(@PathVariable Long historialId, @RequestBody Historial historial) {
         if (historialServices.read(historialId).isPresent()) {
@@ -56,6 +94,15 @@ public class HistorialController {
                 .body("Registro de historial no encontrado");
     }
 
+    @Operation (
+            summary = "Eliminar un registro de historial",
+            description = "Elimina un registro de historial",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Registro de historial eliminado"),
+                    @ApiResponse(responseCode = "404", description = "Registro de historial no encontrado"),
+                    @ApiResponse(responseCode = "500", description = "Error interno")
+            }
+    )
     @DeleteMapping("/{historialId}")
     public ResponseEntity<String> delete(@PathVariable Long historialId) {
         if (historialServices.read(historialId).isPresent()) {
