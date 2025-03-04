a4a6e278232a9e6ee0ebe4b49fa2ab771a756e58
Ruben838 <adam23rvalcarce@inslaferreria.cat>
2025-03-04 17:46:44 +0100

    Modificacion Packages

--- src/main/java/cat/ferreria/api/bussiness/model/UsuarioDTO.java
+++ src/main/java/cat/ferreria/api/bussiness/model/UsuarioDTO.java
@@ -1,46 +1,21 @@
 package cat.ferreria.api.bussiness.model;
+
+import lombok.*;
+
 /**
  * @author Ruben
  * @date 18/02/2025
  */
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
     private String dni;
     private String nombre;
     private String correoElectronico;
 
-    public UsuarioDTO() {
-    }
-
-    public UsuarioDTO(String dni, String nombre, String correoElectronico) {
-        this.dni = dni;
-        this.nombre = nombre;
-        this.correoElectronico = correoElectronico;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCorreoElectronico() {
-        return correoElectronico;
-    }
-
-    public void setCorreoElectronico(String correoElectronico) {
-        this.correoElectronico = correoElectronico;
-    }
-    
     public static class UsuarioMapper {
         public static UsuarioDTO toDTO(Usuario usuario) {
             return new UsuarioDTO(
