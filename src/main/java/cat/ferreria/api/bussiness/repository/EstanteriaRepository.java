25a9d7710b3ca451b8936971ad6d4d93c19678b2
Oriol Cárdenas <47286375+UriBokchoi@users.noreply.github.com>
2025-03-28 17:53:35 +0100

    hyjfhtgtc

--- src/main/java/cat/ferreria/api/bussiness/repository/EstanteriaRepository.java
+++ src/main/java/cat/ferreria/api/bussiness/repository/EstanteriaRepository.java
@@ -1,5 +1,6 @@
 package cat.ferreria.api.bussiness.repository;
 
+import cat.ferreria.api.bussiness.model.clazz.Estante;
 import cat.ferreria.api.bussiness.model.clazz.Estanteria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -17,5 +18,6 @@ public interface EstanteriaRepository extends JpaRepository<Estanteria, Long> {
 
     // Busca estanterías cuyo nombre contenga la cadena dada, ignorando mayúsculas/minúsculas
     List<Estanteria> findByNombreContainingIgnoreCase(String nombre);
+    List<Estante> findEstanteByEstanteriaId(Long estanteriaId);
 
 }
