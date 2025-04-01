e219cf3c73f89a3d2c4ae4edc452e3b5da859e9a
AlexLopezGarcia <alex.lopez.vortex@gmail.com>
2025-04-01 18:18:40 +0200

    Funciona

--- src/main/java/cat/ferreria/api/bussiness/model/clazz/Libro.java
+++ src/main/java/cat/ferreria/api/bussiness/model/clazz/Libro.java
@@ -24,7 +24,7 @@ public class Libro implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "libro_id")
+    @Column(name = "libro_id", nullable = false)
     private Long libroId;
 
     @Column(name = "isbn")
--- src/main/java/cat/ferreria/api/configs/MvcConfiguration.java
+++ src/main/java/cat/ferreria/api/configs/MvcConfiguration.java
@@ -1,24 +1,39 @@
 package cat.ferreria.api.configs;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
-    /* LANGUAGUE CONFIGURATION */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(new Locale("es"));
-        return slr;
+        return new CookieLocaleResolver();
+    }
+
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        return messageSource;
     }
 
     @Bean
@@ -28,10 +43,4 @@ public class MvcConfiguration implements WebMvcConfigurer {
         return lci;
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-    }
-
-    /* END LANGUAGUE CONFIGURATION */
 }
