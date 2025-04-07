23b324d1425f455e0a195187e2bc5a5be0fe7f2c
Sebastian Rengifo <sebastianarengifog16@gmail.com>
2025-04-07 16:01:36 +0200

    Configuración de JWT agregada, ahora se han de cambiar los endpoint a /private /public dependiendo del controlador, siempre si es private han de incluir en los headers el token usando key: Authorization, value: Bearer y concat(token)

---
+++ src/main/java/cat/ferreria/api/configs/security/JwtTokenProvider.java
@@ -0,0 +1,45 @@
+package cat.ferreria.api.configs.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final long expirationInMs = 3600000; // 1 hora
+
+    public String createToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationInMs);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
